661efee2c91da753cc8737cf03a81a4b3aa335be
lakshshinde001 <laxmanshinde3105@gmail.com>
2025-02-27 13:47:52 +0530

    first commit

---
+++ src/constants/socials.tsx
@@ -0,0 +1,15 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+
+export const socials = [
+  
+  {
+    href: "https://www.linkedin.com/in/laxman-shinde-2aa221235/",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  
+];
---
+++ src/constants/timeline.tsx
@@ -0,0 +1,27 @@
+export const timeline = [
+  {
+    company: "AbhinavDCS",
+    title: "Full Stack Developer Intern",
+    date: "Jan 2025 - Present",
+    description: "Building the future of interaction",
+    responsibilities: [
+      "Developing a new product that improves the messaging experience",
+      "Implemented a OCR functionalities to read content from images",
+      "Enhanced the checkout process making it more intuitive and user-friendly",
+      "Built a feature raw images to image that can select text",
+    ],
+  },
+  {
+    company: "Algorithm Tech",
+    title: "Trainer Mentor",
+    date: "Jan 2025 - Present",
+    description: "Cultivating new minds",
+    responsibilities: [
+      "Conducted structured training sessions on Data Structures and Algorithms (DSA)",
+      "covered topics like  trees, graphs, sorting, searching, and dynamic programming.",
+      "Successfully trained 15+ students in DSA and C++",
+      "Mentored students to achieve high ratings on platforms like LeetCode, CodeChef, and CodeForces",
+    ],
+  },
+ 
+];
